cc8122909c6264c9467093a1bfce27140edc20d7
Oleksii <l4212550@gmail.com>
2025-05-21 14:25:46 +0300

    end pixel perfect

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -5,7 +5,7 @@ import "../sass/components/ContactUs.scss";
 const ContactUs = () => {
     return (
         <section className="contact-section">
-            <Container>
+            <Container className="main">
                 <Row className="text-center mb-5">
                     <Col>
                         <p className="section-subtitle">STAY IN TOUCH</p>
@@ -15,19 +15,19 @@ const ContactUs = () => {
                 </Row>
                 <Row className="contact-info text-center">
                     <Col md={4} className="mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"
-                             className="lucide lucide-phone-icon lucide-phone">
-                            <path
-                                d="M13.832 16.568a1 1 0 0 0 1.213-.303l.355-.465A2 2 0 0 1 17 15h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2A18 18 0 0 1 2 4a2 2 0 0 1 2-2h3a2 2 0 0 1 2 2v3a2 2 0 0 1-.8 1.6l-.468.351a1 1 0 0 0-.292 1.233 14 14 0 0 0 6.392 6.384"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round"
+                             className="lucide lucide-smartphone-icon lucide-smartphone">
+                            <rect width="14" height="20" x="5" y="2" rx="2" ry="2"/>
+                            <path d="M12 18h.01"/>
                         </svg>
                         <p>Phone: (415) 124-5678</p>
                         <p>Fax: (412) 123-8290</p>
                     </Col>
                     <Col md={4} className="mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"
-                             className="lucide lucide-map-pin-icon lucide-map-pin">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="0.9545454545454546" stroke-linecap="round"
+                             stroke-linejoin="round" className="lucide lucide-map-pin-icon lucide-map-pin">
                             <path
                                 d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/>
                             <circle cx="12" cy="10" r="3"/>
@@ -35,9 +35,9 @@ const ContactUs = () => {
                         <p>1001 Brickell Bay Dr.<br/>Suite 1900<br/>Miami, FL 33131</p>
                     </Col>
                     <Col md={4} className="mb-4">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2.25" stroke-linecap="round" stroke-linejoin="round"
-                             className="lucide lucide-mail-icon lucide-mail">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="0.9545454545454546" stroke-linecap="round"
+                             stroke-linejoin="round" className="lucide lucide-mail-icon lucide-mail">
                             <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"/>
                             <rect x="2" y="4" width="20" height="16" rx="2"/>
                         </svg>
@@ -74,7 +74,9 @@ const ContactUs = () => {
                 </Container>
             </div>
 
-            <div className="map-background"></div>
+            <div className="map-container">
+                <img src="/src/assets/img/map.jpg" alt="Map" className="map-image" />
+            </div>
         </section>
     );
 };
