3ba1e1f53cb3bc61f30c9cd324f63a9b29a09aa7
Oleksii <l4212550@gmail.com>
2025-05-23 12:27:59 +0300

    gulp edit

--- gulpfile.cjs
+++ gulpfile.cjs
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 
+const sass = require('gulp-sass')(require('sass'));
+
 gulp.task('scripts', function () {
     return gulp.src('src/js/**/*.js')
         .pipe(concat('main.js'))
@@ -13,6 +15,24 @@ gulp.task('images', function () {
     return gulp.src('src/assets/img/**/*')
         .pipe(gulp.dest('dist/images'));
 });
+gulp.task('html', function () {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+});
+const browserSync = require('browser-sync').create();
+
+gulp.task('serve', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    gulp.watch('src/sass/**/*.scss', gulp.series('sass')).on('change', browserSync.reload);
+    gulp.watch('src/js/**/*.js', gulp.series('scripts')).on('change', browserSync.reload);
+    gulp.watch('src/**/*.html', gulp.series('html')).on('change', browserSync.reload);
+});
+
 
 // Головний таск, який запускає лише scripts та images
 gulp.task('default', gulp.series('scripts', 'images'));
